9697cd07d72b74940319bfa07942decce2d0d1f0
Catalin Marinas <catalin.marinas@arm.com>
2023-10-24 11:15:16 +0100

    arm64: pgtable: Replace 'l' with 'lvl' as not to be confused with '1'
    If you are getting older and don't have the right glasses (or a better
    font), 'l' and '1' can be easily confused. Replace the former with
    'lvl'.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/kernel-pgtable.h
+++ arch/arm64/include/asm/kernel-pgtable.h
@@ -49,8 +49,8 @@
 #define EARLY_ENTRIES(vstart, vend, shift, add) \
 	(SPAN_NR_ENTRIES(vstart, vend, shift) + (add))
 
-#define EARLY_LEVEL(l, lvls, vstart, vend, add)	\
-	(lvls > l ? EARLY_ENTRIES(vstart, vend, SWAPPER_BLOCK_SHIFT + l * (PAGE_SHIFT - 3), add) : 0)
+#define EARLY_LEVEL(lvl, lvls, vstart, vend, add)	\
+	(lvls > lvl ? EARLY_ENTRIES(vstart, vend, SWAPPER_BLOCK_SHIFT + lvl * (PAGE_SHIFT - 3), add) : 0)
 
 #define EARLY_PAGES(lvls, vstart, vend, add) (1 	/* PGDIR page */				\
 	+ EARLY_LEVEL(3, (lvls), (vstart), (vend), add) /* each entry needs a next level page table */	\
